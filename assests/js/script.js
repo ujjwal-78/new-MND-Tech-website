6b8d21f1d1fea45ca9dc26b430b4f587aef8b99c
ujjwal-78 <us.ujjwal78@gmail.com>
2021-06-14 13:28:40 +0530

    all pages

--- assests/js/script.js
+++ assests/js/script.js
@@ -1,43 +1,51 @@
 $(document).ready(function () {
+  // sidemenu stARTS
+  $(".sidemenu-toggler").click(function () {
+    $(".sidemenu").addClass("active");
+  });
+  $(".close").click(function () {
+    $(".sidemenu").removeClass("active");
+  });
+  //sidemenu ends
 
-    // sidemenu stARTS
-    $(".sidemenu-toggler").click(function () {
-      $(".sidemenu").addClass("active");
-    });
-    $(".close").click(function () {
-      $(".sidemenu").removeClass("active");
-    });
-    //sidemenu ends
+  // customer-logos sTARTs
+  $(".customer-logos").slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 1199,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
+  // customer-logos eNDs
 
-    // customer-logos sTARTs
-    $(".customer-logos").slick({
-      slidesToShow: 5,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 1500,
-      arrows: false,
-      dots: false,
-      pauseOnHover: false,
-      responsive: [
-        {
-          breakpoint: 1199,
-          settings: {
-            slidesToShow: 4,
-          },
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 2,
-          },
-        },
-        {
-          breakpoint: 520,
-          settings: {
-            slidesToShow: 1,
-          },
-        },
-      ],
-    });
-    // customer-logos eNDs
+  //sticky-business-forms sTARTs
+  $(window).load(function () {
+    ".sticky-form".addClass("active");
+  });
+  $(".cancel").click(function () {
+    $(".sticky-form").removeClass("active");
+  });
+  //sticky-business-forms eNDs
 });
